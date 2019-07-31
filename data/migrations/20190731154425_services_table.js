96986ef50c5e1e77a11465173c71748035b678e0
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-07-31 17:09:06 -0400

    I added the restricted middleware file, I adjusted the SQL table, and I filled in the migration files.

--- data/migrations/20190731154425_services_table.js
+++ data/migrations/20190731154425_services_table.js
@@ -1,8 +1,22 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('services', services => {
+    services
+        .string('username', 260)
+        .foreign('username', 'users')
+        .notNullable();
+    services
+        .string('service name', 260)
+        .notNullable();
+    services
+        .string('email', 260)
+        .notNullable();
+    services
+        .string('password', 260)
+        .notNullable();
+});
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('services');
 };
